703cb5553e877f3930c160b5c9b86049a781a490
Ulf Andrae <ulf.andrae@smhi.se>
2023-02-24 10:51:52 +0100

    Ulf Andrae: Two minor fixes (#110)
    Co-authored-by: Ulf Andrae <snh@ac6-100.bullx>

--- deode/suites.py
+++ deode/suites.py
@@ -146,10 +146,6 @@ class SuiteDefinition(object):
         # EcflowSuiteTask("Background", family, self.task_settings, ecf_files,
         #                input_template=input_template)
 
-        static_data = EcflowSuiteFamily(
-            "StaticData", self.suite, ecf_files
-        )
-
         static_data = EcflowSuiteFamily("StaticData", self.suite, ecf_files)
 
         pgd_input = EcflowSuiteFamily("PgdInput", static_data, ecf_files)
