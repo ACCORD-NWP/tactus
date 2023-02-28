599e8415769b7ad37e876910b71e48e70116af19
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2023-02-28 21:52:48 +0100

    Some selective mocking for tests with tasks

--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Unit tests for the config file parsing module."""
+import subprocess
 from pathlib import Path
 
 import pytest
@@ -9,6 +10,10 @@ import deode
 from deode.config_parser import ParsedConfig, read_raw_config_file
 from deode.discover_task import discover, get_task
 from deode.tasks.base import Task
+from deode.tasks.batch import BatchJob
+from deode.tasks.e923 import E923
+from deode.tasks.forecast import FirstGuess, Forecast
+from deode.toolbox import ArchiveError, FileManager, ProviderError
 
 WORKING_DIR = Path.cwd()
 
@@ -76,6 +81,71 @@ def task_name_and_configs(request, base_raw_config, tmp_path):
     return task_name, task_config
 
 
+@pytest.fixture(scope="module")
+def _module_mockers(session_mocker):
+    original_batchjob_run_method = BatchJob.run
+    original_toolbox_filemanager_input_method = FileManager.input
+    original_task_forecast_forecast_execute_method = Forecast.execute
+    original_task_forecast_firstguess_execute_method = FirstGuess.execute
+    original_task_e923_constant_part_method = E923.constant_part
+    original_task_e923_monthly_part_method = E923.monthly_part
+
+    def new_batchjob_run_method(*args, **kwargs):
+        try:
+            original_batchjob_run_method(*args, **kwargs)
+        except subprocess.CalledProcessError:
+            pass
+
+    def new_toolbox_filemanager_input_method(*args, **kwargs):
+        try:
+            original_toolbox_filemanager_input_method(*args, **kwargs)
+        except (ArchiveError, ProviderError, NotImplementedError):
+            pass
+
+    def new_task_forecast_forecast_execute_method(*args, **kwargs):
+        try:
+            original_task_forecast_forecast_execute_method(*args, **kwargs)
+        except FileNotFoundError:
+            pass
+
+    def new_task_forecast_firstguess_execute_method(*args, **kwargs):
+        try:
+            original_task_forecast_firstguess_execute_method(*args, **kwargs)
+        except FileNotFoundError:
+            pass
+
+    def new_task_e923_constant_part_method(*args, **kwargs):
+        with open("Const.Clim", "w"):
+            original_task_e923_constant_part_method(*args, **kwargs)
+
+    def new_task_e923_montly_part_method(self, constant_file):
+        with open(constant_file, "w"):
+            original_task_e923_monthly_part_method(self, constant_file)
+
+    session_mocker.patch(
+        "deode.toolbox.FileManager.input", new=new_toolbox_filemanager_input_method
+    )
+    session_mocker.patch("deode.tasks.batch.BatchJob.run", new=new_batchjob_run_method)
+    session_mocker.patch(
+        "deode.tasks.forecast.Forecast.execute",
+        new=new_task_forecast_forecast_execute_method,
+    )
+    session_mocker.patch(
+        "deode.tasks.forecast.FirstGuess.execute",
+        new=new_task_forecast_firstguess_execute_method,
+    )
+
+    # TODO: Mock deode.tasks.data.OutputData.data instead
+    session_mocker.patch("deode.tasks.data.OutputData.archive_files", return_value=None)
+    session_mocker.patch(
+        "deode.tasks.e923.E923.constant_part", new=new_task_e923_constant_part_method
+    )
+    session_mocker.patch(
+        "deode.tasks.e923.E923.monthly_part", new=new_task_e923_montly_part_method
+    )
+    session_mocker.patch("surfex.SURFEXBinary")
+
+
 class TestTasks:
     # pylint: disable=no-self-use
     """Test all tasks."""
@@ -84,6 +154,7 @@ class TestTasks:
         class_name, task_config = task_name_and_configs
         assert isinstance(get_task(class_name, task_config), Task)
 
+    @pytest.mark.usefixtures("_module_mockers")
     def test_task_can_be_run(self, task_name_and_configs):
         class_name, task_config = task_name_and_configs
         my_task_class = get_task(class_name, task_config)
