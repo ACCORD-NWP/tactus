2890691a69e55a76451fb8de5e061e030a9fc6b2
cv6 (Alex Deckmyn) <alex.deckmyn@meteo.be>
2023-12-05 20:36:50 +0000

    Fix SQLite extraction: speed, combined fields, refactoring

--- conftest.py
+++ conftest.py
@@ -3,75 +3,83 @@
 import itertools
 import sys
 
+import numpy as np
+
 
 class MockObject(object):
     # use this to return objects with attributes
     pass
 
 
-def mock_getstring(_msgid, key):
-    if key == "level":
-        return "0"
+def mock_codes_get_string(msgid, key):
+    # in reality msgid should be a grib handle
+    # in the mocker, it will be a dict
+    if key in msgid.keys():
+        return str(msgid[key])
     else:
         return "ok"
 
 
-def mock_getlong(_msgid, key):
-    if key == "productDefinitionTemplateNumber":
-        return 8
-    if key == "indicatorOfUnitOfTimeRange":
-        return 13
-    if key == "indicatorOfUnitForTimeRange":
-        return 13
+def mock_codes_get_long(msgid, key):
+    if key in msgid.keys():
+        return int(msgid[key])
     else:
         return 0
 
 
-def mock_getdouble(_msgid, _key):
-    return 1.0
+def mock_codes_get_double(msgid, key):
+    if key in msgid.keys():
+        return float(msgid[key])
+    else:
+        return 0.0
 
 
-def mock_gethandle(fileobj):
-    # NOTE: this mocker expects an intertools.count object!
-    x = fileobj.__next__()
-    if x < 3:
-        return x
+def mock_codes_grib_new_from_file(fileobj):
+    # NOTE: we return a dict with test values
+    # but the tests also need to stop
+    # fileobj should be a file with 1 byte or so
+    testgid = {
+        "shortName": "t",
+        "productDefinitionTemplateNumber": 8,
+        "indicatorOfUnitOfTimeRange": 13,
+        "indicatorOfUnitForTimeRange": 13,
+        "Nx": 2,
+        "Ny": 2,
+        "level": 0,
+        "typeOfLevel": "isobaricInhPa",
+        "dataDate": "20230915",
+        "dataTime": "000000",
+        "forecastTime": 0,
+        "lengthOfTimeRange": 0,
+        "gridType": "lambert",
+        "Latin1InDegrees": 50.0,
+        "Latin2InDegrees": 50.0,
+        "LoVInDegrees": 0.0,
+        "DxInMetres": 1,
+        "DyInMetres": 1,
+    }
+    if fileobj.read(1):
+        return testgid
     else:
         return None
 
 
-def mock_iterator(*_args, **_kwargs):
-    return itertools.count(1)
+def mock_codes_get_values(gribid):
+    nx = int(gribid["Nx"])
+    ny = int(gribid["Ny"])
+    return np.array([0] * nx * ny)
 
 
-def mock_iterator_next(gribid):
-    # NOTE: this mocker expects an intertools.count object!
-    x = gribid.__next__()
-    if x < 3:
-        return [0.0, 0.0]
-    else:
-        return None
-
-
-def mock_nearest(*_args, **kwargs):
-    x = MockObject()
-    x.distance = 1.0
-    x.index = 0
-    return [x] * kwargs["npoints"]
-
-
-def mock_values(*_args, **_kwargs):
-    return [0, 0]
+class MockKeyValueNotFoundError(Exception):
+    pass
 
 
 mock_eccodes = type(sys)("eccodes")
-mock_eccodes.codes_get_string = mock_getstring
-mock_eccodes.codes_get_long = mock_getlong
-mock_eccodes.codes_get_double = mock_getdouble
-mock_eccodes.codes_grib_new_from_file = mock_gethandle
-mock_eccodes.codes_grib_iterator_new = mock_iterator
-mock_eccodes.codes_grib_iterator_next = mock_iterator_next
-mock_eccodes.codes_grib_find_nearest = mock_nearest
-mock_eccodes.codes_get_values = mock_values
+mock_eccodes.codes_get_string = mock_codes_get_string
+mock_eccodes.codes_get_long = mock_codes_get_long
+mock_eccodes.codes_get_double = mock_codes_get_double
+mock_eccodes.codes_grib_new_from_file = mock_codes_grib_new_from_file
+mock_eccodes.codes_get_values = mock_codes_get_values
+mock_eccodes.KeyValueNotFoundError = MockKeyValueNotFoundError
 
 sys.modules["eccodes"] = mock_eccodes
