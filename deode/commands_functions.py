96f908602ae8ec1ee63164d434133a355586e4a7
draelsaid <aelsaid@hotmail.co.uk>
2023-12-19 15:18:58 +0000

    minor format changes. want to sync online with local

--- deode/commands_functions.py
+++ deode/commands_functions.py
@@ -102,10 +102,14 @@ def start_suite(args, config):
     # Check if user passed flags, and reiterate ecf vars to them
     logger.info(
         "ecf_jobout: {}", config["scheduler.ecfvars.ecf_jobout"]
-    ) if args.joboutdir is None else logger.info("ecf_jobout: {}", args.joboutdir)
+    ) if args.joboutdir is None else logger.info(
+        "ecf_jobout: {}", args.joboutdir
+    )
     logger.info(
         "ecf_files: {}", config["scheduler.ecfvars.ecf_files"]
-    ) if args.ecf_files is None else logger.info("ecf_files: {}", args.ecf_files)
+    ) if args.ecf_files is None else logger.info(
+        "ecf_files: {}", args.ecf_files
+    )
     logger.info(
         "ecf_files_remotely: {}", config["scheduler.ecfvars.ecf_files_remotely"]
     ) if args.ecf_files_remotely is None else logger.info(
@@ -113,7 +117,9 @@ def start_suite(args, config):
     )
     logger.info(
         "ecf_home: {}", config["scheduler.ecfvars.ecf_home"]
-    ) if args.ecf_home is not None else logger.info("ecf_home: {}", args.ecf_home)
+    ) if args.ecf_home is not None else logger.info(
+        "ecf_home: {}", args.ecf_home
+    )
 
     server = EcflowServer(config, start_command=args.start_command)
 
