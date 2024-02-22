2149dc2ff265dae1ff465d7b3a8e116d84b387f8
Nika Kastelec <sink@ac6-102.bullx>
2024-02-22 13:37:08 +0000

    test suite

--- deode/suites.py
+++ deode/suites.py
@@ -352,7 +352,7 @@ class SuiteDefinition(object):
             if prev_interpolation_trigger is not None:
                 triggers = triggers + prev_interpolation_trigger
             ready_for_marsprep = EcflowSuiteTriggers(triggers)
-            if self.do_marsprep:
+            if self.do_marsprep and self.interpolate_boundaries:
                 EcflowSuiteTask(
                     "Marsprep",
                     inputdata,
@@ -365,10 +365,8 @@ class SuiteDefinition(object):
                     ecf_files_remotely=self.ecf_files_remotely,
                 )
 
-            if not self.surfex:
-                self.do_prep = False
 
-            if self.interpolate_boundaries or self.do_prep:
+            if self.interpolate_boundaries:
                 int_fam = EcflowSuiteFamily(
                     f'{"Interpolation"}',
                     time_family,
@@ -393,65 +391,64 @@ class SuiteDefinition(object):
                     if self.mode != "cold_start":
                         self.do_prep = False
 
-                if self.interpolate_boundaries:
-                    basetime = as_datetime(cycle["basetime"])
-                    forecast_range = as_timedelta(config["general.times.forecast_range"])
-                    endtime = basetime + forecast_range
-                    bdint = as_timedelta(config["boundaries.bdint"])
-                    bdmax = config["boundaries.bdtasks_per_batch"]
+                basetime = as_datetime(cycle["basetime"])
+                forecast_range = as_timedelta(config["general.times.forecast_range"])
+                endtime = basetime + forecast_range
+                bdint = as_timedelta(config["boundaries.bdint"])
+                bdmax = config["boundaries.bdtasks_per_batch"]
 
-                    bdnr = 0
-                    intnr = 1
-                    args = ""
-                    int_trig = inputdata_done
-                    bdtime = basetime
-                    intbdint = int(bdint.total_seconds() // 3600)
-                    while bdtime <= endtime:
-                        bch_fam = EcflowSuiteFamily(
-                            f"Batch{intnr:02}",
-                            int_fam,
-                            self.ecf_files,
-                            trigger=int_trig,
-                            variables=None,
-                            ecf_files_remotely=self.ecf_files_remotely,
-                        )
-                        while bdtime <= endtime:
-                            date_string = bdtime.isoformat(sep="T").replace("+00:00", "Z")
-                            args = f"bd_time={date_string};bd_nr={bdnr}"
-                            variables = {"ARGS": args}
-                            lbc_fam = EcflowSuiteFamily(
-                                f"LBC{bdnr*intbdint:02}",
-                                bch_fam,
-                                self.ecf_files,
-                                trigger=None,
-                                variables=None,
-                                ecf_files_remotely=self.ecf_files_remotely,
-                            )
+                bdnr = 0
+                intnr = 1
+                args = ""
+                int_trig = inputdata_done
+                bdtime = basetime
+                intbdint = int(bdint.total_seconds() // 3600)
+                while bdtime <= endtime:
+                   bch_fam = EcflowSuiteFamily(
+                       f"Batch{intnr:02}",
+                       int_fam,
+                       self.ecf_files,
+                       trigger=int_trig,
+                       variables=None,
+                       ecf_files_remotely=self.ecf_files_remotely,
+                   )
+                   while bdtime <= endtime:
+                       date_string = bdtime.isoformat(sep="T").replace("+00:00", "Z")
+                       args = f"bd_time={date_string};bd_nr={bdnr}"
+                       variables = {"ARGS": args}
+                       lbc_fam = EcflowSuiteFamily(
+                           f"LBC{bdnr*intbdint:02}",
+                           bch_fam,
+                           self.ecf_files,
+                           trigger=None,
+                           variables=None,
+                           ecf_files_remotely=self.ecf_files_remotely,
+                       )
 
-                            interpolation_task = "c903" if self.do_marsprep else "e927"
+                       interpolation_task = "c903" if self.do_marsprep else "e927"
 
-                            EcflowSuiteTask(
-                                interpolation_task,
-                                lbc_fam,
-                                config,
-                                self.task_settings,
-                                self.ecf_files,
-                                input_template=input_template,
-                                variables=variables,
-                                trigger=None,
-                                ecf_files_remotely=self.ecf_files_remotely,
-                            )
+                       EcflowSuiteTask(
+                           interpolation_task,
+                           lbc_fam,
+                           config,
+                           self.task_settings,
+                           self.ecf_files,
+                           input_template=input_template,
+                           variables=variables,
+                           trigger=None,
+                           ecf_files_remotely=self.ecf_files_remotely,
+                       )
 
-                            bdnr += 1
-                            bdtime += bdint
-                            if bdnr % bdmax == 0:
-                                intnr += 1
-                                int_trig = EcflowSuiteTriggers(
-                                    [EcflowSuiteTrigger(bch_fam)]
-                                )
-                                break
+                       bdnr += 1
+                       bdtime += bdint
+                       if bdnr % bdmax == 0:
+                           intnr += 1
+                           int_trig = EcflowSuiteTriggers(
+                               [EcflowSuiteTrigger(bch_fam)]
+                           )
+                           break
 
-                    int_trig = EcflowSuiteTriggers([EcflowSuiteTrigger(int_fam)])
+                   int_trig = EcflowSuiteTriggers([EcflowSuiteTrigger(int_fam)])
             else:
                 int_trig = inputdata_done
 
