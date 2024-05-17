f3aefc91e6166d4e6139a6620cb486f0495f21b9
Denis Haumont <denis.haumont@meteo.be>
2024-05-17 17:45:20 +0200

    Tools to convert namelist.yaml

---
+++ deode/namelist_generation_input/convert_namelist.py
@@ -0,0 +1,71 @@
+import yaml
+import os
+import sys
+import copy
+if len(sys.argv) < 2:
+    print("Usage - directive file yaml and yaml and yaml file")
+    exit(1)
+
+tnt_directive_filename =sys.argv[1]
+namelist_filename =sys.argv[2]
+
+
+with open(tnt_directive_filename, mode="rt", encoding="utf-8") as file:
+    tnt_directives = yaml.safe_load(file)
+    file.close()
+
+with open(namelist_filename, mode="rt", encoding="utf-8") as file:
+    namelist = yaml.safe_load(file)
+    file.close()
+
+
+new_namelist = copy.deepcopy(namelist)
+
+if "new_blocks" in tnt_directives:
+    for new_block in tnt_directives["new_blocks"]:    
+        print("Create block", new_block)
+        for namelists_section in namelist:
+            for namelist_block in namelist[namelists_section]:                 
+                if new_block in namelist_block:                                
+                    print("ERROR: Block existing")
+                else:
+                    if (not new_block in new_namelist[namelists_section]):
+                        print("Create block ", new_block)
+                        new_namelist[namelists_section][new_block] = dict()                                    
+
+
+if "blocks_to_move" in tnt_directives:
+    for blocks in tnt_directives["blocks_to_move"]:    
+        print("Move block", blocks)
+
+if "keys_to_remove" in tnt_directives:
+    for blocks in tnt_directives["keys_to_remove"]:    
+        print("Delete key", blocks)
+                    
+        for namelists_section in namelist:
+            for namelist_block in namelist[namelists_section]:                            
+                if blocks in namelist_block:                                
+                    print("Block found")
+
+
+if "keys_to_move" in tnt_directives:
+    for old_block in tnt_directives["keys_to_move"]:
+        for old_key in tnt_directives["keys_to_move"][old_block]:
+            for new_block in tnt_directives["keys_to_move"][old_block][old_key]:            
+                    new_key=tnt_directives["keys_to_move"][old_block][old_key][new_block]                    
+                    
+                    for namelists_section in namelist:
+                        for namelist_block in namelist[namelists_section]:                            
+                            if old_block in namelist_block:                                
+                                if old_key in namelist[namelists_section][namelist_block]:                                                                        
+                                    if not new_block in new_namelist[namelists_section]:
+                                        print("Create block ", new_block)
+                                        new_namelist[namelists_section][new_block] = dict()                                    
+                                    print("Add Key: ", new_block,"\\", new_key, ":", namelist[namelists_section][old_block][old_key])
+                                    new_namelist[namelists_section][new_block][new_key]=namelist[namelists_section][old_block][old_key]
+                                    print("Delete Key: ", old_block,"\\", old_key)
+                                    del new_namelist[namelists_section][old_block][old_key]
+        
+            
+
+exit(0)
