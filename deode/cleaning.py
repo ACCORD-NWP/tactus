29b14daaa53d09f7e1226e408133462e3bafe0e3
Ulf Andrae <ulf.andrae@smhi.se>
2024-06-15 06:40:51 +0200

    Correcting the cleaning (#656)
    * Ulf Andrae: Unit testing
    
    * Ulf Andrae: Linting
    
    ---------
    
    Co-authored-by: Ulf Andrae <snh@ac6-100.bullx>
    Co-authored-by: Ulf.Andrae <a000864@c22681.ad.smhi.se>
    Co-authored-by: Ulf Andrae <snh@ac6-102.bullx>

--- deode/cleaning.py
+++ deode/cleaning.py
@@ -20,8 +20,17 @@ class CleanDeode:
             defaults (dict): Default cleaning settings
             basetime (dateTime object): Reference time
 
+        Raises:
+            RuntimeError: If erroneous defaults
+
         """
-        self.defaults = {} if defaults is None else defaults
+        if defaults is None:
+            self.defaults = {}
+        elif isinstance(defaults, dict):
+            self.defaults = defaults
+        else:
+            self.defaults = defaults.dict()
+
         self.basetime = (
             as_datetime(config["general.times.basetime"])
             if basetime is None
@@ -29,6 +38,7 @@ class CleanDeode:
         )
         self.cycle_length = as_timedelta(config["general.times.cycle_length"])
         self.platform = Platform(config)
+        self._check_choice(self.defaults, "defaults")
 
     def _set_defaults(self, choice):
         """Copy default settings.
@@ -41,28 +51,45 @@ class CleanDeode:
 
         """
         x = choice.copy()
-        for k, v in self.defaults.items():
+        y = self.defaults.copy()
+
+        # Do not copy competing settings
+        if "ncycles_delay" in y and "cleaning_delay" in x:
+            y.pop("ncycles_delay")
+        if "ncycles_delay" in x and "cleaning_delay" in y:
+            y.pop("cleaning_delay")
+
+        for k, v in y.items():
             if k not in choice:
                 x[k] = v
 
         return x
 
-    def _check_delay(self, delay, tag):
-        """Check if that a value is a multiple of cycle_length.
+    def _check_choice(self, choice, name=""):
+        """Check choices.
 
         Args:
-            delay (timeDelta object): Timedelta to check
-            tag (str): Name for error message
+            choice (dict): Dict with cleaning settings
+            name (str): Optional name for better logging
 
         Raises:
-            RuntimeError: If not divisible by cycle_length
+            RuntimeError:
 
         """
-        if delay % self.cycle_length != as_timedelta("PT0H"):
-            logger.error(f"{tag} must be a multiple of cycle_length")
-            logger.error(f"{tag}:{delay}")
-            logger.error(f"cycle_length:{self.cycle_length}")
-            raise RuntimeError
+        if "cleaning_delay" in choice and "ncycles_delay" in choice:
+            raise RuntimeError(
+                f"Define either ncycles_delay or cleaning_delay for {name}"
+            )
+
+        for tag in ["cleaning_delay", "cleanin_max_delay", "step"]:
+            if tag not in choice:
+                continue
+            delay = choice[tag]
+            if as_timedelta(delay) % self.cycle_length != as_timedelta("PT0H"):
+                logger.error(f"{tag} must be a multiple of cycle_length")
+                logger.error(f"{tag}:{delay}")
+                logger.error(f"cycle_length:{self.cycle_length}")
+                raise RuntimeError
 
     def prep_cleaning(self, choices, basetime=None):
         """Prepare tasks for cleaning.
@@ -71,27 +98,23 @@ class CleanDeode:
             choices (dict): Dict with cleaning settings
             basetime (dateTime object): Reference time
 
-        Raises:
-            RuntimeError: If erroneous choices
-
         """
         if basetime is not None:
             self.basetime = basetime
 
-        self.clean_tasks = []
-        for _choice in choices.values():
+        self.clean_tasks = {}
+        for name, _choice in choices.items():
             choice = self._set_defaults(_choice)
             if choice["active"]:
+                choice.pop("active")
+                self.clean_tasks[name] = choice
                 # Check consistency of settings
-
-                if "cleaning_delay" in choice and "ncycles_delay" in choice:
-                    raise RuntimeError("Define either ncycles_delay or cleaning_delay")
 
                 if "cleaning_delay" not in choice:
                     choice["cleaning_delay"] = choice["ncycles_delay"] * self.cycle_length
+                    choice.pop("ncycles_delay")
                 else:
                     choice["cleaning_delay"] = as_timedelta(choice["cleaning_delay"])
-                self._check_delay(choice["cleaning_delay"], "cleaning_delay")
 
                 if "cleaning_max_delay" not in choice:
                     choice["cleaning_max_delay"] = choice["cleaning_delay"]
@@ -99,20 +122,19 @@ class CleanDeode:
                     choice["cleaning_max_delay"] = as_timedelta(
                         choice["cleaning_max_delay"]
                     )
-                    self._check_delay(choice["cleaning_max_delay"], "cleaning_max_delay")
 
                 if "step" not in choice:
                     choice["step"] = self.cycle_length
                 else:
                     choice["step"] = as_timedelta(choice["step"])
-                    self._check_delay(choice["step"], "step")
 
-                self.clean_tasks.append(choice)
+                self._check_choice(choice, name)
+                self.clean_tasks[name] = choice
 
     def clean(self):
         """Perform the cleaning."""
-        for choice in self.clean_tasks:
-            logger.info("Cleaning choice:{}", choice)
+        for name, choice in self.clean_tasks.items():
+            logger.info("Cleaning {}:{}", name, choice)
 
             basetime = self.basetime - choice["cleaning_delay"]
             endtime = self.basetime - choice["cleaning_max_delay"]
