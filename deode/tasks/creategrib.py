fb43a1beeab2b94c4e181f0734052f7cf5992ef9
Tommaso Benacchio <56339853+tbnc@users.noreply.github.com>
2024-10-29 21:42:40 +0100

    Removing leftover commented out section from creategrib file

--- deode/tasks/creategrib.py
+++ deode/tasks/creategrib.py
@@ -92,14 +92,6 @@ class CreateGrib(Task):
                 filetype_map = self.rules[filetype]["filetype_map"]
             except KeyError:
                 filetype_map = filetype
-            #           file_handle = FileManager.create_list(
-            #    self,
-            #    self.basetime,
-            #    self.forecast_range,
-            #    self.file_templates[filetype]["archive"],
-            #    self.output_settings[filetype_map],
-            #)
-            #
             file_handle = self.create_list(
                 self.file_templates[filetype]["archive"],
                 self.output_settings[filetype_map],
