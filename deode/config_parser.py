5d5159024cb1875866fe7fe1f5a212cfb5fddb3b
Ulf.Andrae <a000864@c22681.ad.smhi.se>
2025-01-11 07:21:12 +0100

    Ulf Andrae: Validation fixes

--- deode/config_parser.py
+++ deode/config_parser.py
@@ -298,13 +298,18 @@ class ParsedConfig(BasicConfig):
         """Initialise an instance with an arbitrary number of entries & validate them."""
         self.include_dir = include_dir
         self.json_schema = json_schema
-        self.validate_json_schema = len(json_schema) > 0
         self.host = host
         super().__init__(*args, **kwargs)
 
     @BasicConfig.data.setter
     def data(self, new):
-        """Set the underlying data stored by the instance."""
+        """Set the underlying data stored by the instance.
+
+        Skip the validation if the class is instantiated with an empty schema.
+
+        """
+        validate_json_schema = len(self.json_schema) > 0
+
         new, json_schema = _expand_config_include_section(
             raw_config=new,
             json_schema=self.json_schema,
@@ -313,26 +318,21 @@ class ParsedConfig(BasicConfig):
         )
         ParsedConfig.json_schema.fset(self, json_schema, _validate_data=False)
 
-        if self.validate_json_schema:
-            # Make sure all sections defined in the schema
-            # are also present in the new config
-            sections_that_should_not_be_defaulted = [
-                "include",
-                *new,
-                *json_schema.get("required", []),
-            ]
-            for property_name, property_schema in json_schema.get(
-                "properties", {}
-            ).items():
-                if property_name in sections_that_should_not_be_defaulted:
-                    continue
-                if property_schema.get("type", "") == "object":
-                    new[property_name] = {}
+        # Make sure all sections defined in the schema are also present in the new config
+        sections_that_should_not_be_defaulted = [
+            "include",
+            *new,
+            *json_schema.get("required", []),
+        ]
+        for property_name, property_schema in json_schema.get("properties", {}).items():
+            if property_name in sections_that_should_not_be_defaulted:
+                continue
+            if property_schema.get("type", "") == "object":
+                new[property_name] = {}
 
-            BasicConfig.data.fset(self, self.json_schema.validate(new))
-        else:
-            BasicConfig.data.fset(self, new)
-            self.validate_json_schema = True
+        if validate_json_schema:
+            new = self.json_schema.validate(new)
+        BasicConfig.data.fset(self, new)
 
     @property
     def include_dir(self):
