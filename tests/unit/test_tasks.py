ee39fbabd85e1d6447b3cd0593099bb779fc459c
Martin Frølund <maf@dmi.dk>
2026-02-20 09:26:08 +0000

    Re-introduce cleaning test_tasks

--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -2,8 +2,10 @@
 """Unit tests for the config file parsing module."""
 import contextlib
 import subprocess
+import sys
 from os import chdir, makedirs
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 import tomlkit
@@ -14,6 +16,7 @@ from tactus.plugin import TactusPluginRegistry
 from tactus.tasks.archive import ArchiveHour, ArchiveStatic
 from tactus.tasks.base import Task
 from tactus.tasks.batch import BatchJob
+from tactus.tasks.clean_old_data import CleanCases
 from tactus.tasks.collectlogs import CollectLogs
 from tactus.tasks.creategrib import GlGrib
 from tactus.tasks.discover_task import available_tasks, get_task
@@ -26,6 +29,9 @@ from tactus.tasks.iomerge import IOmerge
 from tactus.tasks.marsprep import Marsprep
 from tactus.tasks.sqlite import ExtractSQLite, MergeSQLites
 from tactus.toolbox import ArchiveError, FileManager, ProviderError
+
+with contextlib.suppress(ModuleNotFoundError):
+    import ecflow
 
 
 def classes_to_be_tested():
@@ -88,6 +94,7 @@ def _mockers_for_task_run_tests(session_mocker, tmp_path_factory):
     original_task_forecast_firstguess_execute_method = FirstGuess.execute
     original_task_archive_archivehour_execute_method = ArchiveHour.execute
     original_task_archive_archivestatic_execute_method = ArchiveStatic.execute
+    original_task_clean_old_data_cleancases_execute_method = CleanCases.execute
     original_task_creategrib_glgrib_execute_method = GlGrib.execute
     original_task_gribmodify_addtotalprec_execute_method = AddCalculatedFields.execute
     original_task_extractsqlite_extractsqlite_execute_method = ExtractSQLite.execute
@@ -119,6 +126,15 @@ def _mockers_for_task_run_tests(session_mocker, tmp_path_factory):
         """Suppress some errors so that test continues if they happen."""
         with contextlib.suppress(ArchiveError, ProviderError, NotImplementedError):
             original_toolbox_filemanager_input_method(*args, **kwargs)
+
+    def new_task_clean_old_data_cleancases_execute_method(*args, **kwargs):
+        """Suppress some errors so that test continues if they happen."""
+        with contextlib.suppress(ModuleNotFoundError, NotImplementedError):
+            if "ecflow" in sys.modules:
+                with patch.object(ecflow.Client, "delete"):
+                    original_task_clean_old_data_cleancases_execute_method(
+                        *args, **kwargs
+                    )
 
     def new_task_forecast_forecast_execute_method(*args, **kwargs):
         """Suppress some errors so that test continues if they happen."""
@@ -217,6 +233,10 @@ def _mockers_for_task_run_tests(session_mocker, tmp_path_factory):
     session_mocker.patch("tactus.tasks.batch.BatchJob.run", new=new_batchjob_run_method)
     session_mocker.patch(
         "tactus.toolbox.FileManager.input", new=new_toolbox_filemanager_input_method
+    )
+    session_mocker.patch(
+        "tactus.tasks.clean_old_data.CleanCases.execute",
+        new=new_task_clean_old_data_cleancases_execute_method,
     )
     session_mocker.patch(
         "tactus.tasks.forecast.Forecast.execute",
