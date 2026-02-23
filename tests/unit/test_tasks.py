a1d7fd3c751f823c995af10a76c253f7fd6dbfde
Martin Frølund <maf@dmi.dk>
2026-02-23 11:30:23 +0100

    Remove clean_old_data.py

--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -16,7 +16,6 @@ from tactus.plugin import TactusPluginRegistry
 from tactus.tasks.archive import ArchiveHour, ArchiveStatic
 from tactus.tasks.base import Task
 from tactus.tasks.batch import BatchJob
-from tactus.tasks.clean_old_data import CleanCases
 from tactus.tasks.collectlogs import CollectLogs
 from tactus.tasks.creategrib import GlGrib
 from tactus.tasks.discover_task import available_tasks, get_task
@@ -94,7 +93,6 @@ def _mockers_for_task_run_tests(session_mocker, tmp_path_factory):
     original_task_forecast_firstguess_execute_method = FirstGuess.execute
     original_task_archive_archivehour_execute_method = ArchiveHour.execute
     original_task_archive_archivestatic_execute_method = ArchiveStatic.execute
-    original_task_clean_old_data_cleancases_execute_method = CleanCases.execute
     original_task_creategrib_glgrib_execute_method = GlGrib.execute
     original_task_gribmodify_addtotalprec_execute_method = AddCalculatedFields.execute
     original_task_extractsqlite_extractsqlite_execute_method = ExtractSQLite.execute
@@ -233,10 +231,6 @@ def _mockers_for_task_run_tests(session_mocker, tmp_path_factory):
     session_mocker.patch("tactus.tasks.batch.BatchJob.run", new=new_batchjob_run_method)
     session_mocker.patch(
         "tactus.toolbox.FileManager.input", new=new_toolbox_filemanager_input_method
-    )
-    session_mocker.patch(
-        "tactus.tasks.clean_old_data.CleanCases.execute",
-        new=new_task_clean_old_data_cleancases_execute_method,
     )
     session_mocker.patch(
         "tactus.tasks.forecast.Forecast.execute",
