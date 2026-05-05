9ca17750188004906b171fdc065f134a049299a5
Ulf.Andrae <a000864@c22681.ad.smhi.se>
2026-05-05 14:23:57 +0200

    Ulf Andrae: Fix failing unit test

--- tests/unit/test_reference_checker.py
+++ tests/unit/test_reference_checker.py
@@ -25,7 +25,9 @@ from tactus.toolbox import FileManager
 @pytest.fixture(scope="module")
 def basic_config(default_config):
     config = default_config
-    return config.copy(update=set_times(config))
+    config = config.copy(update=set_times(config))
+    config = config.copy(update={ "general" : { "cycle": "CY49t2" }})
+    return config
 
 
 def configure_for_check_and_generate(
