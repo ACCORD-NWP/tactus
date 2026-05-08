4d79de253d0769916de8a5b25895dd4a7c49aae9
pardallio <pardalmiguel@hotmail.com>
2026-05-08 15:32:35 +0000

    adds option to use build cache dir

--- tactus/tasks/compilation.py
+++ tactus/tasks/compilation.py
@@ -5,7 +5,15 @@ import sys
 
 import yaml
 
+import hashlib
+import json
+from pathlib import Path
+
+from git import Repo, InvalidGitRepositoryError
+
+
 from ..os_utils import tactusmakedirs
+from ..logs import logger
 from .base import Task
 from .batch import BatchJob
 
@@ -88,13 +96,29 @@ class TactusBundleBuild(Task):
         """
         Task.__init__(self, config, __class__.__name__)
 
-        compile_dir = self.config["compile.dir"]
-        self.compile_dir = self.platform.substitute(compile_dir)
+        bundle_dir = self.config["compile.dir"]
+        self.bundle_dir = self.platform.substitute(bundle_dir)
         self.ecbundle_bin = f"{os.path.dirname(sys.executable)}/ecbundle"
 
         self.arch = self.config["compile.arch"]
-        bindir = "@CASEDIR@/install"
-        builddir = "@CASEDIR@/build"
+
+        if self.config["compile.cache"]:
+            self.bundle_hash = self.get_bundle_hash(f"{self.bundle_dir}/source")
+            # get arch
+            arch_dir=Path(f"{self.bundle_dir}/{self.arch}")
+            default_link = arch_dir / "default"
+            if default_link.exists() and default_link.is_symlink():
+                arch=str(default_link.resolve())
+            else:
+                arch=str(arch_dir)
+
+            compile_dir=f"{self.config['compile.cache_dir']}/{arch}/{self.bundle_hash}"
+    
+        else:
+            compile_dir="@CASEDIR@"
+
+        bindir = f"{compile_dir}/install"    
+        builddir = f"{compile_dir}/build"
         bindir = self.platform.substitute(bindir)
         bindir = os.path.realpath(bindir)
         builddir = self.platform.substitute(builddir)
@@ -103,15 +127,73 @@ class TactusBundleBuild(Task):
         self.exp_builddir = builddir
         tactusmakedirs(self.exp_bindir)
         tactusmakedirs(self.exp_builddir)
+
         self.ninja_arg = ""
         if self.config["compile"].get("ninja"):
             self.ninja_arg = "--ninja "
+
+    def get_bundle_hash(self,source_dir):
+        
+        logger.debug("Build a hash for the source bundle")
+
+        manifest = {
+            "repositories": {},
+            "dirty": False,
+        }
+        source_path = Path(source_dir)
+        # Iterate through immediate subfolders
+        for folder in sorted(source_path.iterdir()):
+            if not folder.is_dir():
+                continue
+
+            try:
+                repo = Repo(folder)
+            except InvalidGitRepositoryError:
+                logger.info("[SKIP] Not a git repo: {}", folder.name)
+                continue
+                
+            logger.info("[CHECK] {}", folder.name)
+
+            # test for modified/staged/untracked files:
+            dirty = repo.is_dirty(untracked_files=True)
+
+            repo_info = {
+                "commit": repo.head.commit.hexsha,
+                "dirty": dirty,
+            }   
+
+            manifest["repositories"][folder.name] = repo_info
+
+            if repo_info["dirty"]:
+                manifest["dirty"] = True
+
+        # Deterministic serialization
+        serialized = json.dumps(
+            manifest,
+            sort_keys=True,
+            separators=(",", ":"),
+        )
+
+        # Combined deterministic hash
+        build_hash = hashlib.sha256(
+            serialized.encode("utf-8")
+        ).hexdigest()
+
+        # Mark hash as dirty if any repo is dirty
+        if manifest["dirty"]:
+            build_hash += "-dirty"
+
+        logger.info(f"hash for {source_path}: {build_hash}")
+
+        return build_hash
+
+
 
     def execute(self):
         """Execute task."""
         batch_job = BatchJob(os.environ)
         batch_job.run(
-            f"cd {self.compile_dir};  {self.ecbundle_bin} build "
+            f"cd {self.bundle_dir};  {self.ecbundle_bin} build "
             + f"--arch {self.arch} {self.ninja_arg} --forecast-only "
             + f"--install-dir={self.exp_bindir} --install "
             + f"--build-dir={self.exp_builddir}"
