ac3a0f23cabadf7a0e663549fe8eae3af6d979e9
Nika Kastelec <sink@ac6-102.bullx>
2026-05-27 12:07:19 +0000

    fix logger restor sys_name

--- tactus/__init__.py
+++ tactus/__init__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Package to run the Destination Earth on Demand Extremes system."""
 
+import os
+import sys
 from importlib.metadata import version
 from pathlib import Path
 
 from .aux_types import QuasiConstant
-import os
-import sys
+
 
 class GeneralConstants(QuasiConstant):
     """General package-related constants."""
@@ -14,6 +15,3 @@ class GeneralConstants(QuasiConstant):
     PACKAGE_NAME = __name__
     VERSION = version(__name__)
     PACKAGE_DIRECTORY = Path(__file__).parent
-    PACKAGE_FILE = os.path.basename(sys.argv[0] if sys.argv else __file__)
-    
-
--- tactus/logs.py
+++ tactus/logs.py
@@ -90,16 +90,16 @@ class LoggerHandlers(Sequence):
         return len(self.handlers)
 
 
-def log_elapsed_time(package_name=GeneralConstants.PACKAGE_NAME, version=GeneralConstants.VERSION, **kwargs):
+def log_elapsed_time(
+    package_name=GeneralConstants.PACKAGE_NAME, version=GeneralConstants.VERSION, **kwargs
+):
     """Return a decorator that logs beginning, exit and elapsed time of function."""
 
     def log_elapsed_time_decorator(function):
         """Wrap `function` and log beginning, exit and elapsed time."""
         name = kwargs.get("name", function.__name__)
-        logger.info("not main:{}", name)
         if function.__name__ == "main":
             name = f"{package_name} v{version}"
-            logger.info("if main:{}", name)
             cmd = f"{' '.join([package_name, *sys.argv[1:]])}"
             name = f'{name} --> "{cmd}"'
 
