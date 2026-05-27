fa0693c852af322cc9a4e12e3781c437eef70508
Nika Kastelec <sink@ac6-102.bullx>
2026-05-27 07:23:54 +0000

    print names

--- tactus/logs.py
+++ tactus/logs.py
@@ -97,10 +97,12 @@ def log_elapsed_time(package_name=GeneralConstants.PACKAGE_NAME, version=General
         """Wrap `function` and log beginning, exit and elapsed time."""
         name = kwargs.get("name", function.__name__)
         if function.__name__ == "main":
+            logger.info("if main:", name)
             name = f"{package_name} v{version}"
             cmd = f"{' '.join([package_name, *sys.argv[1:]])}"
             name = f'{name} --> "{cmd}"'
 
+        logger.info("else:", name)
         @wraps(function)
         def wrapper(*args, **kwargs):
             logger.opt(colors=True).info("<blue>Start {}</blue>", name)
