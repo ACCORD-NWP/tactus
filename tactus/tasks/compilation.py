22df13d377b36c52f39558dbf6fe4ce120897651
pardallio <pardalmiguel@hotmail.com>
2026-05-27 21:49:06 +0000

    correct for test

--- tactus/tasks/compilation.py
+++ tactus/tasks/compilation.py
@@ -143,7 +143,8 @@ class TactusBundleCreate(Task):
 
                 with open(self.update_bundle_file, "r", encoding="utf-8") as f:
                     upd_bundle_dict = yaml.load(f) or {}
-            except: FileNotFoundError
+            
+            except FileNotFoundError:
                 orig_bundle_dict = {}
                 upd_bundle_dict = {}
 
