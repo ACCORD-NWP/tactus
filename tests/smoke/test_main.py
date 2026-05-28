4cbc5a20e7c19902ebd78330c56c0e36b96ab3b7
Martin Frølund <maf@dmi.dk>
2026-05-28 14:47:02 +0000

    Linting

--- tests/smoke/test_main.py
+++ tests/smoke/test_main.py
@@ -91,7 +91,6 @@ def _module_mockers(module_mocker, config_path, tmp_path_factory: pytest.TempPat
 
 
 def test_package_executable_is_in_path():
-    __import__("ipdb").set_trace()
     assert shutil.which(GeneralConstants.PACKAGE_NAME)
 
 
