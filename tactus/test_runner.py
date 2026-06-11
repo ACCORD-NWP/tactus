f7ce3bdc7ea5122417af3b70ec426a139a9165b2
Denis Haumont <denis.haumont@meteo.be>
2026-06-11 12:22:04 +0000

    collect summaries when no argument is provided

--- tactus/test_runner.py
+++ tactus/test_runner.py
@@ -575,4 +575,7 @@ def run_test(args, config=None):
         t.list()
 
     elif args.config_file is not None:
-        t.execute(args)
+        if args.configure or args.run:
+            t.execute(args)
+        else:
+            t.collect_summaries()
