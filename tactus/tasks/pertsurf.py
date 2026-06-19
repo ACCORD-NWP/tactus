35707defa1c7460d0ba84928d84b9f0f327d302c
Xiaohui Zhao <zhao@vedur.is>
2026-06-19 10:49:23 +0000

    Add PertSurf

---
+++ tactus/tasks/pertsurf.py
@@ -0,0 +1,95 @@
+"""PertSurf"""
+
+import os
+
+from tactus.logs import logger
+from tactus.datetime_utils import as_datetime
+
+from tactus.initial_conditions import InitialConditions
+from tactus.tasks.base import Task
+from tactus.tasks.batch import BatchJob
+
+
+class PertSurf(Task):
+    """Perturb surface parameters in SURFEX initial file"""
+
+    def __init__(self, config):
+        """Construct PertSurf object.
+
+        Args:
+            config (tactus.ParseConfig): Configuration object.
+        """
+
+        Task.__init__(self, config, __class__.__name__)
+
+        self.npatch = self.config["pgd.npatch"]
+        self.archive = self.platform.get_system_value("archive")
+        self.basetime = as_datetime(self.config["general.times.basetime"])
+        self.pertsurf_param = self.config.get_as_dict("perturbations.pertsurf_param")
+
+        self.binary = self.get_binary("PERTSURF")
+
+    @staticmethod
+    def fmt_fortran_val(value):
+        if isinstance(value, bool):
+            return ".true." if value else ".false."
+        if isinstance(value, str):
+            return f"'{value}'" 
+        if isinstance(value, (list, tuple)):
+            return ", ".join(
+                f"'{v}'" if isinstance(v, str) else str(v) for v in value
+                )
+        return str(value)
+    
+    def build_namelist(self, param_dict, iseed, output):
+        """Build namelist string for PERTSURF."""
+        param_dict["ISEED"] = iseed
+        param_dict["CFSFC"] = output
+        param_dict["IPATCH"] = self.npatch
+
+        namelist_str = "&NAMSFC\n"
+        for key, value in param_dict.items():
+            formatted_value = self.fmt_fortran_val(value)
+            namelist_str += f"{key.upper()} = {formatted_value},\n"
+        namelist_str += "/\n"
+        return namelist_str
+    
+    def execute(self):
+
+        ensmbr = int(os.environ.get("MEMBER"))
+
+        # Surface initial file
+        _, initfile_sfx = InitialConditions(self.config).find_initial_files()
+
+        initfile_sfx_name = os.path.basename(initfile_sfx)
+        output = f"{initfile_sfx_name}_perturbed"
+        self.fmanager.input(
+            initfile_sfx, 
+            output,
+            provider_id="copy",
+            )
+
+        # Seed
+        M = 100000
+        dtg = int(self.basetime.strftime("%Y%m%d%H"))
+        iseed = M * ensmbr + (dtg % M)
+        logger.info(f"Generated seed (ISEED): {iseed}")
+
+        # Build namelist
+        namelist_str = self.build_namelist(self.pertsurf_param, iseed, output)
+
+        with open("nampert", "w") as nml:
+            nml.write(namelist_str)
+
+        logger.info("Created namelist: {}", "nampert")
+
+        # Run PERTSURF 
+        batch = BatchJob(os.environ, wrapper=self.wrapper)
+        batch.run(self.binary)
+
+        # Move output to archive
+        self.fmanager.output(
+            output,
+            f"{self.archive}/{output}",
+            provider_id="move",
+            )
