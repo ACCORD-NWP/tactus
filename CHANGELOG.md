aab82ea4dafeb0bdaf202a020ce14d5f56a48264
Haumont Denis <76622294+dhaumont@users.noreply.github.com>
2026-06-29 11:11:01 +0200

    Test runner - display summary of all tests (#129)
    * implement summary
    
    * fix merge error
    
    * change the filename of config definition file
    
    * fix unit test
    
    * changelog
    
    * add skipped
    
    * Linter
    
    ---------
    
    Co-authored-by: Denis Haumont <denis.haumont@meteo.be>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Introduce perturbation family and config section. [#87](https://github.com/ACCORD-NWP/tactus/pull/87) (@uandrae)
 - Tactus test runner - Fix underscore in test definition. [#127](https://github.com/ACCORD-NWP/tactus/pull/127) (@dhaumont)
 - ALARO: Use APL_ALARO routines instead of APLPAR. [#135](https://github.com/ACCORD-NWP/tactus/pull/135) (@dhaumont)
+- Test runner - display summary of all tests. [#129](https://github.com/ACCORD-NWP/tactus/pull/129/)(@dhaumont)
 
 ### Changed
 - Let the DKCOEXP be the default large domain. [\#116](https://github.com/ACCORD-NWP/tactus/pull/116)(@uandrae)
--- tactus/test_runner.py
+++ tactus/test_runner.py
@@ -5,9 +5,11 @@ import concurrent.futures
 import contextlib
 import copy
 import glob
+import json
 import os
 import shutil
 import tempfile
+from datetime import datetime
 from pathlib import Path
 
 import tomli
@@ -20,6 +22,8 @@ from .fullpos import flatten_list
 from .general_utils import merge_dicts
 from .host_actions import TactusHost
 from .logs import logger
+from .reference_checker import CheckSummaryAnalysis
+from .toolbox import Platform
 
 
 class TestCases:
@@ -42,6 +46,7 @@ class TestCases:
         if args.config_file is not None:
             logger.info("Using config file: {}", args.config_file)
             self.config = ParsedConfig.from_file(args.config_file, json_schema={})
+            self.config_name = Path(args.config_file).resolve().stem
             try:
                 definitions = self.config.expand_macros().dict()
             except KeyError:
@@ -277,7 +282,7 @@ class TestCases:
         from .__main__ import main as tactus_main
 
         try:
-            with open(f"{self.test_dir}/config_names.toml", "rb") as f:
+            with open(f"{self.test_dir}/{self.config_name}_config_names.toml", "rb") as f:
                 config_names = tomli.load(f)
         except FileNotFoundError as err:
             msg = "No case mapping available. Run again without '-r'"
@@ -446,6 +451,105 @@ class TestCases:
                 self.cases[case]["hostname"] = hostnames[item["host"]]["config_name"]
                 self.cases[case]["hostdomain"] = hostnames[item["host"]]["domain_name"]
 
+    @staticmethod
+    def get_case_information(config_file):
+        """Get case name, json file and reference folder from the config file.
+
+        Arguments:
+            config_file (str): Path to the config file
+
+        Returns:
+            case_name name: the name of the case.
+            json_file: the json file path
+            references_folder: the references folder path.
+        """
+        case_config = ParsedConfig.from_file(config_file, json_schema={})
+        platform = Platform(case_config)
+
+        case_name = platform.substitute(
+            case_config.get("general.case"),
+            basetime=case_config["general.times.start"],
+            validtime=case_config["general.times.start"],
+        )
+        json_file = platform.substitute(
+            case_config.get("reference_checker.summary.json.file"),
+            basetime=case_config["general.times.start"],
+            validtime=case_config["general.times.start"],
+        )
+        references_folder = platform.get_platform_value("references_folder")
+        check = case_config["reference_checker"]["check"]
+
+        return check, case_name, json_file, references_folder
+
+    def collect_summaries(self):
+        """Collect summaries from the runs."""
+        try:
+            with open(f"{self.test_dir}/{self.config_name}_config_names.toml", "rb") as f:
+                config_names = tomli.load(f)
+        except FileNotFoundError as err:
+            msg = "No case mapping available. Run again with '-m'"
+            logger.error(msg)
+            raise FileNotFoundError(msg) from err
+
+        config_files = [
+            f"{self.test_dir}/{config_name}.toml"
+            for config_name in config_names["config_names"].values()
+        ]
+
+        summaries = {}
+        case_files = {}
+        width = 0
+        now = datetime.now()
+        skipped = set()
+        for config_file in config_files:
+            check, case_name, json_file, references_folder = (
+                TestCases.get_case_information(config_file)
+            )
+            if check:
+                case_files[case_name] = json_file
+                try:
+                    with open(json_file, "r", encoding="utf-8") as f:
+                        summary = json.load(f)
+                        summary["creation_date"] = datetime.fromtimestamp(
+                            os.path.getmtime(json_file)
+                        )
+                except FileNotFoundError:
+                    summary = "MISSING"
+
+                summaries[case_name] = summary
+            else:
+                summaries[case_name] = "SKIPPED"
+                skipped.add(case_name)
+            width = max(width, len(case_name))
+
+        if len(summaries) > 0:
+            with contextlib.suppress(KeyError):
+                logger.info("Our version {}", self.ial["ial_hash"])
+            with contextlib.suppress(KeyError):
+                logger.info(" from {}", self.ial["pr"])
+            logger.info("Comparison against {}", references_folder)
+
+            case_names = sorted([x for x in summaries if x in skipped])
+            case_names.extend(sorted([x for x in summaries if x not in skipped]))
+            for case_name in case_names:
+                summary = summaries[case_name]
+                creation_date = (
+                    summary["creation_date"] if not isinstance(summary, str) else None
+                )
+                colored_message = CheckSummaryAnalysis.colored_result_message(
+                    summary,
+                    self.verbose,
+                    case_name,
+                    case_files.get(case_name) if case_name in case_files else None,
+                    width,
+                    creation_date,
+                    now,
+                )
+                logger.opt(colors=True).info(colored_message)
+
+            if not self.verbose:
+                logger.opt(colors=True).info("<blue>Add '-v' for more info</blue>")
+
     def execute(self, args):
         """Execute test cases.
 
@@ -475,7 +579,7 @@ class TestCases:
                         for c, item in self.cases.items()
                         if "config_name" in item
                     }
-                }).save_as(f"{directory}/config_names.toml")
+                }).save_as(f"{directory}/{self.config_name}_config_names.toml")
 
             if not args.run:
                 logger.info("\n\nRerun with '-r' to start the suites\n\n")
@@ -494,6 +598,9 @@ def run_test(args, config=None):
     """
     t = TestCases(args=args)
 
+    if not args.config_file and not args.prepare_binaries and not args.list:
+        logger.warning("Nothing to do. Use `tactus test -h` for help.")
+        return False
     if args.prepare_binaries:
         t.get_binaries()
 
@@ -501,4 +608,9 @@ def run_test(args, config=None):
         t.list()
 
     elif args.config_file is not None:
-        t.execute(args)
+        if args.configure or args.run:
+            t.execute(args)
+        else:
+            t.collect_summaries()
+
+    return True
